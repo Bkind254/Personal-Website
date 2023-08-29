16b4c406d10fc261654afa556be6e89e0d5e39b9
Bkind254 <langatbrian02@gmail.com>
2023-08-29 09:53:49 +0300

    3rd commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,8 +4,8 @@ import { FaGithub } from "react-icons/fa";
 import IMG1 from "../../assets/project1.JPG";
 import IMG2 from "../../assets/project2.JPG";
 import IMG3 from "../../assets/project3.JPG";
-import IMG4 from "../../assets/project4.JPG";
-import IMG5 from "../../assets/project5.JPG";
+import IMG4 from "../../assets/hero.JPG";
+import IMG5 from "../../assets/shoshy.JPG";
 import IMG7 from "../../assets/project7.JPG";
 
 function Portfolio() {
@@ -14,67 +14,6 @@ function Portfolio() {
       <h2>Projects</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="item3" />
-          </div>
-          <h4 className="title">CraftsmenCreatives</h4>
-          <p className="project-content">
-            Welcome to the home of art.This website connects you to one of the
-            top artists in Nakuru.
-          </p>
-
-          <ul>HTML , CSS, Bootstrap, Javascript</ul>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Bkind254/CRAFTSMEN-CREATIVES-KE-RESPONSIVE"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-            <a
-              href="https://craftsmencreatives-ke.netlify.app"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Check it out
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG7} alt="item5" />
-          </div>
-          <h4 className="title">Otaku2</h4>
-          <p className="project-content">
-            Otaku2 lets anime fans search for any anime and read a concise
-            synopsis. Ideal for those who want a quick overview without
-            committing to a full series.
-          </p>
-
-          <ul>React CSS</ul>
-          <div className="portfolio__item-cta">
-            <a
-              href="https://github.com/Bkind254/Otaku"
-              className="btn"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-            <a
-              href="https://otaku2.netlify.app"
-              className="btn btn-primary"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Check it out
-            </a>
-          </div>
-        </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
             <img src={IMG1} alt="item1" />
@@ -110,10 +49,9 @@ function Portfolio() {
           <div className="portfolio__item-image">
             <img src={IMG4} alt="item4" />
           </div>
-          <h4 className="title">Food Website</h4>
+          <h4 className="title">Hero Match</h4>
           <p className="project-content">
-            This is a food/restaurant websites that helps showcase its food,
-            products and services.
+            This is a simple game website. Feel free to play a little😉.
           </p>
 
           <ul>React, CSS</ul>
@@ -127,7 +65,7 @@ function Portfolio() {
               <FaGithub />
             </a>
             <a
-              href="https://ugali.netlify.app"
+              href="https://heromatch.netlify.app/"
               className="btn btn-primary"
               rel="noreferrer"
             >
@@ -139,12 +77,11 @@ function Portfolio() {
           <div className="portfolio__item-image">
             <img src={IMG5} alt="item5" />
           </div>
-          <h4 className="title">Store 42</h4>
+          <h4 className="title">SHOSHY</h4>
           <p className="project-content">
-            An online shop using a sleek and modern interface, featuring a
-            shopping cart, products, and secure payment processing. The project
-            was developed to create an e-commerce space and the backend is
-            managed with Sanity.
+            An online shop using a sleek and modern interface. The project was
+            developed to create an e-commerce space and the backend is managed
+            with Sanity.
           </p>
 
           <ul>Next Js , CSS</ul>
@@ -158,7 +95,7 @@ function Portfolio() {
               <FaGithub />
             </a>
             <a
-              href="https://store-42.vercel.app/"
+              href="https://shoshy.netlify.app/"
               className="btn btn-primary"
               target="_blank"
               rel="noreferrer"
